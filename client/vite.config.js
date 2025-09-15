68884768c5ac9913266a3bfdedc3844cf41b1e58
eddaww <cai01edwin@gmail.com>
2025-09-15 04:10:31 -0700

    some css deployment update

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../dist',   // 默认值（生成在 client/dist）
+  },
 })
--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 import express from "express";
 import axios from "axios";
 import cors from "cors";
+import { fileURLToPath } from "url"; 
+import path from "path";
 
 const app = express();
 app.use(cors());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../dist")));
 
 app.get("/balloons/:hour", async (req, res)=>{
   const hour = req.params.hour|| "00";
@@ -14,6 +21,10 @@ app.get("/balloons/:hour", async (req, res)=>{
     console.error(err.message);
     res.status(500).json({error: "Failed to fetch balloon data at hour"});
   }
+});
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
 });
 
 const PORT = 4000;
